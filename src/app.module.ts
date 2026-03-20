86c57123959c889b92250909ced86ed65b67822a
tolarom <tolarom27@gmail.com>
2026-03-21 04:02:19 +0700

    feat: configure TypeORM with PostgreSQL and set up receipts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Receipt } from './database/entities/receipts.entity';
+import { ReceiptsController } from './receipts/receipts.controller';
+import { ReceiptsService } from './receipts/receipts.service';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'nachoo',
+      database: 'nest_js_tp',
+      entities: [Receipt],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Receipt]),
+  ],
+  controllers: [AppController, ReceiptsController],
+  providers: [AppService, ReceiptsService],
 })
 export class AppModule {}
