420c8d20dd31d369af1430662fc9a16cb6d80429
tolarom <tolarom27@gmail.com>
2026-03-30 17:03:57 +0700

    feat: update AppModule to include Orders and Notifications modules, and add Order entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,12 @@ import { Receipt } from './database/entities/receipts.entity';
 import { ReceiptsController } from './receipts/receipts.controller';
 import { ReceiptsService } from './receipts/receipts.service';
 import { ConfigModule } from '@nestjs/config';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ReceiptsModule } from './receipts/receipts.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
+import { Order } from './database/entities/order.entity';
 
 ConfigModule.forRoot();
 
@@ -18,12 +24,15 @@ ConfigModule.forRoot();
       username: 'postgres',
       password: 'nachoo',
       database: 'nest_js_tp',
-      entities: [Receipt],
+      entities: [Receipt, Order],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Receipt]),
+    TypeOrmModule.forFeature([Receipt, Order]),
+    NotificationsModule,
+    ReceiptsModule,
+    OrdersModule,
   ],
-  controllers: [AppController, ReceiptsController],
-  providers: [AppService, ReceiptsService],
+  controllers: [AppController, ReceiptsController, OrdersController],
+  providers: [AppService, ReceiptsService, OrdersService],
 })
 export class AppModule {}
